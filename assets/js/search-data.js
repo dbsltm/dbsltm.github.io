57909dbe8c64b257beee85e88bdc4d82e122dda6
dbsltm <eunkyu0322@gmail.com>
2025-03-07 16:12:14 +0000

    Deploying to gh-pages from @ dbsltm/dbsltm.github.io@e5581bdd2937c57d3489ecfa21bee8a62707d56a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,16 +429,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-paper-halloc-token-level-localization-of-hallucinations-for-vision-language-models-is-accepted-to-cvpr-2025-sparkles-smile",
+          title: 'Our paper, HalLoc: Token-level Localization of Hallucinations for Vision Language Models is accepted...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
