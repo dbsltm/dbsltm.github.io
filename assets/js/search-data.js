e57b3ecb8e271e8202092569ff611f8596daf7b1
dbsltm <eunkyu0322@gmail.com>
2025-03-07 16:06:11 +0000

    Deploying to gh-pages from @ dbsltm/dbsltm.github.io@8287065a96166402d186bce2ec4cd6b22198649e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -521,13 +521,6 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
